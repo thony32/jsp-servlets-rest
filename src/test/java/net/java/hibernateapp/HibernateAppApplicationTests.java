46b354e7c0c4dcc9b70fffa0ffc0b81b99e15fb8
Anthony <mahefasoanyrianaanthony09@gmail.com>
2024-03-25 20:15:26 +0300

    spring

---
+++ src/test/java/net/java/hibernateapp/HibernateAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.java.hibernateapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
